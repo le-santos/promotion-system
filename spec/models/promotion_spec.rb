1a0d6c0c6e639a74213a802f9fcfc7826339a764
le-santos <lequinterio@gmail.com>
2021-01-25 19:39:16 -0300

    adiciona validacao para evitar campos vazios e duplicacao

--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe Promotion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Promotion do
+  context 'validation' do
+    it 'attributes cannot be blank' do
+      promotion = Promotion.new
+
+      promotion.valid?
+
+      expect(promotion.errors[:name]).to include('não pode ficar em branco')
+      expect(promotion.errors[:code]).to include('não pode ficar em branco')
+      expect(promotion.errors[:discount_rate]).to include('não pode ficar em '\
+                                                          'branco')
+      expect(promotion.errors[:coupon_quantity]).to include('não pode ficar em'\
+                                                            ' branco')
+      expect(promotion.errors[:expiration_date]).to include('não pode ficar em'\
+                                                            ' branco')
+    end
+
+    it 'code must be uniq' do
+      Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                        code: 'NATAL10', discount_rate: 10,
+                        coupon_quantity: 100, expiration_date: '22/12/2033')
+      promotion = Promotion.new(code: 'NATAL10')
+
+      promotion.valid?
+
+      expect(promotion.errors[:code]).to include('deve ser único')
+    end
+  end
 end
