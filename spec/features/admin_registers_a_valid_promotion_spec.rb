21f594902ca4823dc044aa1714ddfa8abea4b12b
le-santos <lequinterio@gmail.com>
2021-01-26 10:04:30 -0300

    altera layout e inline style das msgs de erro

---
+++ spec/features/admin_registers_a_valid_promotion_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'Admin registers a valid promotion' do
+  scenario 'and attributes cannot be blank' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    fill_in 'Nome', with: ''
+    fill_in 'Descrição', with: ''
+    fill_in 'Código', with: ''
+    fill_in 'Desconto', with: ''
+    fill_in 'Quantidade de cupons', with: ''
+    fill_in 'Data de término', with: ''
+    click_on 'Criar promoção'
+
+    expect(page).to have_content('não pode ficar em branco', count: 5)
+  end
+
+  scenario 'and code must be unique' do
+    Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033')
+
+    visit root_path
+    click_on 'Promoções'
+    click_on 'Registrar uma promoção'
+    fill_in 'Código', with: 'NATAL10'
+    click_on 'Criar promoção'
+
+    expect(page).to have_content('deve ser único')
+  end
+end
+
