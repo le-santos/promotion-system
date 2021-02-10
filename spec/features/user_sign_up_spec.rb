805e6843e6f4ab1633fdc424cdc40f3cb3f35a42
le-santos <lequinterio@gmail.com>
2021-02-10 11:03:48 -0300

    user sign up

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -15,4 +15,21 @@ feature 'User sign up' do
     expect(page).to have_content('Bem vindo! Você realizou seu registro com sucesso.')
     expect(page).to have_content('joao@email.com')
   end
+
+  scenario 'is canceled if email not unique' do
+    User.create!(email: 'jose@email.com', password: '123456')
+
+    visit root_path
+    click_on 'Inscrever-se'
+    within('form') do
+      fill_in 'E-mail', with: 'jose@email.com'
+      fill_in 'Senha', with: '123456'
+      fill_in 'Confirme sua senha', with: '123456'
+      click_on 'Inscrever-se'
+    end
+
+    expect(page).to have_content('Não foi possível salvar usuário: 1 erro')
+    expect(page).to have_content('E-mail já está em uso')
+    expect(User.all.size).to eq(1)
+  end
 end
